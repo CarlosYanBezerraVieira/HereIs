0744ed219959160551afe3b419b35bb95fedd8c2
CarlosYanBezerraVieira <carlosyanceara@gmail.com>
2023-03-26 12:03:06 -0300

    feature: created section top

---
+++ lib/pages/home/sections/section_top.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:hereis/pages/home/widget/text_field_widget.dart';
+
+import '../widget/card_of_filter.dart';
+
+class SectionTop extends StatelessWidget {
+  const SectionTop({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 30),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(top: 64, bottom: 32),
+                child: TextFieldWidget(),
+              ),
+              Wrap(
+                spacing: 12,
+                children: [
+                  CardOfFilter(
+                    label: 'Todos',
+                    onPressed: () {},
+                    selected: true,
+                  ),
+                  CardOfFilter(
+                    label: 'Histórico',
+                    onPressed: () {},
+                    selected: false,
+                  ),
+                  CardOfFilter(
+                    label: 'Salvos',
+                    onPressed: () {},
+                    selected: false,
+                  ),
+                ],
+              )
+            ],
+          ),
+        ),
+      ],
+    );
+  }
+}
