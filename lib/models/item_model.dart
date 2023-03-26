3aadebb8c8beed22e2657687f472a9824fdd9d91
CarlosYanBezerraVieira <carlosyanceara@gmail.com>
2023-03-26 20:45:48 -0300

    feature: created item model

---
+++ lib/models/item_model.dart
@@ -0,0 +1,18 @@
+class ItemModel {
+  final String cep;
+  final String street;
+  final String city;
+  final String state;
+  final String neighborhood;
+  final String complemento;
+  final String ddd;
+  ItemModel({
+    required this.cep,
+    required this.street,
+    required this.city,
+    required this.state,
+    required this.neighborhood,
+    required this.complemento,
+    required this.ddd,
+  });
+}
