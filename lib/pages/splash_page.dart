674beaff45636e0fc52f31b8efdee0d859447ccf
CarlosYanBezerraVieira <carlosyanceara@gmail.com>
2023-04-23 11:34:38 -0300

    refactor: removed internet check on splash page

--- lib/pages/splash_page.dart
+++ lib/pages/splash_page.dart
@@ -1,4 +1,3 @@
-import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 
@@ -29,96 +28,28 @@ class _SplashPageState extends State<SplashPage> with TickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-        future: isOnline(),
-        builder: (context, online) {
-          if (online.data ?? true) {
-            return Container(
-              color: const Color(0xff38c172),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  LottieBuilder.asset(
-                    Gifs.search,
-                    controller: _controller,
-                    onLoaded: (compos) {
-                      _controller
-                        ..duration = compos.duration
-                        ..forward().then((value) {
-                          Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                  builder: (context) => const HomePage()));
-                        });
-                    },
-                  )
-                ],
-              ),
-            );
-          }
-          return Container(
-            color: const Color(0xff38c172),
-            child: Material(
-              color: Colors.transparent,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Icon(
-                    Icons.error_rounded,
-                    color: Colors.white,
-                    size: 100,
-                  ),
-                  const SizedBox(
-                    height: 16,
-                  ),
-                  const Text(
-                    'Erro na conexão.',
-                    style: TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.w700,
-                      color: Colors.white,
-                    ),
-                  ),
-                  IconButton(
-                    onPressed: () async {
-                      setState(() {
-                        loading = true;
-                      });
-                      Future.delayed(const Duration(seconds: 1), () {
-                        setState(() {
-                          loading = false;
-                        });
-                      });
-                      await isOnline();
-                    },
-                    icon: loading
-                        ? const SizedBox(
-                            height: 30,
-                            width: 30,
-                            child: CircularProgressIndicator(
-                              color: Colors.yellow,
-                            ),
-                          )
-                        : const Icon(
-                            Icons.refresh,
-                            color: Colors.yellow,
-                          ),
-                  )
-                ],
-              ),
-            ),
-          );
-        });
-  }
-
-  Future<bool> isOnline() async {
-    try {
-      final url = Uri.parse('https://viacep.com.br/ws/63031000/json/');
-      await http.get(url).timeout(const Duration(seconds: 1));
-
-      return true;
-    } catch (_) {
-      return false;
-    }
+    return Container(
+      color: const Color(0xff38c172),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          LottieBuilder.asset(
+            repeat: true,
+            Gifs.search,
+            controller: _controller,
+            onLoaded: (compos) {
+              _controller
+                ..duration = compos.duration
+                ..forward().then((value) {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const HomePage()));
+                });
+            },
+          )
+        ],
+      ),
+    );
   }
 }
