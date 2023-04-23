510b07bf6568d9d853c0d5de1106ef3eb406beab
CarlosYanBezerraVieira <carlosyanceara@gmail.com>
2023-04-23 10:51:21 -0300

    refactor: moved card not found to background

--- lib/pages/home/sections/section_background.dart
+++ lib/pages/home/sections/section_background.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+
 import 'package:lottie/lottie.dart';
+
+import 'section_is_empty.dart';
 
 class SectionBackground extends StatelessWidget {
   final List<Widget> children;
@@ -21,11 +24,18 @@ class SectionBackground extends StatelessWidget {
         Container(
           height: height / 2,
           decoration: const BoxDecoration(
-              color: Color(0xff1B1E23),
-              borderRadius: BorderRadius.only(
-                bottomLeft: Radius.circular(30),
-                bottomRight: Radius.circular(30),
-              )),
+            color: Color(0xff1B1E23),
+            borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(30),
+              bottomRight: Radius.circular(30),
+            ),
+          ),
+        ),
+        Positioned(
+          left: 30,
+          right: 30,
+          bottom: (height / 2) + 30,
+          child: const SectionIsEmpty(),
         ),
         Column(
           children: children,
